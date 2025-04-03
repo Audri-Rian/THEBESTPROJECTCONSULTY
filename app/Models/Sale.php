40d70f5330e18246bf777594f8e50b52626ecfa4
Pedro Felix <pedrohfcavalcanti7@gmail.com>
2025-04-03 15:19:10 -0300

    feat: added a function to the Sale model

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -10,4 +10,9 @@ class Sale extends Model
     use HasFactory;
     protected $table = 'sales';
     protected $fillable = ['customer_id', 'total_amount'];
+
+    public function scopeLast30Days(Builder $query)
+    {
+        return $query->whereDate('created_at', '>=', date('Y-m-d', strtotime('-30 days')));
+    }
 }
